2c331f08e3c50aa7cb7a4295826adce5c7ece5f1
pravinpoudel <prvnpoudel4@gmail.com>
2021-09-04 23:22:56 -0600

    change nothing to care of

--- dist/Nims.dev.js
+++ dist/Nims.dev.js
@@ -1,24 +1,30 @@
-let player = null;
+"use strict";
 
-let t0 = performance.now();
+var player = null;
+var t0 = performance.now(); // check if this move is winner of looser
 
-// check if this move is winner of looser
-const checkWin = (n, move = 0) => {
+var checkWin = function checkWin(n) {
+  var move = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
   player = move % 2 == 0 ? "human" : "computer";
+
   if (n == 0) {
-    console.log(`its a win by a ${player} in ${move}th move`);
+    console.log("its a win by a ".concat(player, " in ").concat(move, "th move"));
     return true;
   } else {
     move++;
+
     if (n == 1 || n == 2) {
       return checkWin(n - 1, move);
     }
+
     if (n % 3 == 0) {
       return checkWin(n - 2, move);
     }
+
     if (n % 3 == 2) {
       return checkWin(n - 1, move);
     }
+
     if (n % 3 == 1) {
       return checkWin(n - 2, move);
     }
@@ -26,7 +32,6 @@ const checkWin = (n, move = 0) => {
 };
 
 checkWin(6, 0);
-
-let t1 = performance.now();
-
+var t1 = performance.now();
 console.log("execution time is" + (t2 - t1));
+//# sourceMappingURL=Nims.dev.js.map
